4db3200f4f27ed490e745f31b157d54e10e115d8
Cerf Kristian A <cerfkris@cubbli24-gold.cs.helsinki.fi>
2026-08-18 21:01:38 +0300

    created a basic blog app

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_time")
+    ordering = ("-created_time",)
